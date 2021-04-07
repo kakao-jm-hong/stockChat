4144f74876260d198df820a0a84186ec4b2f4811
HongJeongMin <wjdals0471@naver.com>
2021-04-07 23:57:21 +0900

    eject적용전

---
+++ Front/bit-chat/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.scss';
+import MainContainer from './page/MainContainer'
+function App() {
+  return (
+    <MainContainer />
+  );
+}
+
+export default App;
---
+++ Front/bit-chat/src/page/MainContainer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import style from 'MainContainer.scss';
+class MainContainer extends Component {
+    render() {
+        return (
+            <>
+            <div>
+                main
+            </div>
+            </>
+        );
+    }
+}
+
+export default MainContainer;
