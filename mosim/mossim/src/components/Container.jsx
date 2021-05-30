3d15cebf39d9615756833389a6823998bb280b61
HongJeongMin <wjdals0471@naver.com>
2021-05-30 21:07:59 +0900

    mossim

--- mosim/mossim/src/components/Container.jsx
+++ mosim/mossim/src/components/Container.jsx
@@ -1,45 +1,45 @@
 import React from 'react';
 import classnames from 'classnames';
-import './Container.scss';
-import Item from './item';
-import Pagination from './pagination';
+import style from'./Container.scss';
+// import Item from './item';
+// import Pagination from './pagination';
 
-const DEMO_PROPS = {
-  list_title: '모심',
-  list_type: 'home',
-  login: false
-}
+// const DEMO_PROPS = {
+//   list_title: '모심',
+//   list_type: 'home',
+//   login: false
+// }
 
 class ExampleComponent extends React.Component {
   render() {
-    const { list_title, list_type, login } = DEMO_PROPS;
-    let list_type_class;
+    // const { list_title, list_type, login } = DEMO_PROPS;
+    // let list_type_class;
 
-    switch (list_type) {
-      case 'home':
-      case 'login':
-        list_type_class = 'type1';
-        break;
+    // switch (list_type) {
+    //   case 'home':
+    //   case 'login':
+    //     list_type_class = 'type1';
+    //     break;
 
-      case 'end':
-        list_type_class = 'type2';
-        break;
+    //   case 'end':
+    //     list_type_class = 'type2';
+    //     break;
     
-      default:
-        break;
-    }
+    //   default:
+    //     break;
+    // }
 
-    const list_wrap_class = classnames("list_wrap", list_type_class, {
-      'is-login': login
-    });
+    // const list_wrap_class = classnames("list_wrap", list_type_class, {
+    //   'is-login': login
+    // });
 
     return (
-      <div className={list_wrap_class}>
-        <h2 className="list_title">{list_title}</h2>
-        <div className="list_area">
-          <Item></Item>
-        </div>
-        <Pagination></Pagination>
+      <div className={style.wrap}>
+        <h1>Mossim 공식 페이지</h1>
+        <footer>
+          대표 : 최문수
+          tel : 010-0000-0000
+        </footer>
       </div>
     );
   }
