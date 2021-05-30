6c31b7c047aee9777154e3ce58a3114ddd71b41e
HongJeongMin <wjdals0471@naver.com>
2021-05-30 14:41:28 +0900

    Spring 프로젝트 추가, IOS 폴더추가

---
+++ Back/bitChat/src/main/java/com/bitChat/BitChatApplication.java
@@ -0,0 +1,13 @@
+package com.bitChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitChatApplication.class, args);
+	}
+
+}
---
+++ Back/bitChat/src/test/java/com/bitChat/BitChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
